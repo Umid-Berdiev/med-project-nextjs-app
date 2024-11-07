76fd7e4dd809edf55739893a4df2ebc212cf0767
nodir_b <beshimovnodir@mail.ru>
2024-11-07 17:57:29 +0500

    s

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -7,7 +7,7 @@ export default function DashboardPage() {
   return (
     <div className='container pb-10'>
       <section className='flex flex-col items-center justify-center py-16'>
-        <Heading2>{t('Start your journey')}</Heading2>
+        <Heading2>{t('Start your journeyyy')}</Heading2>
         <p>
           {t(
             'This nextjs template includes theme swicher, lang switcher and tailwindcss with daisyUI'
